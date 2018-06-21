17511536f1ccab9dcce19d9e5d0efea2b009ae0b
Simon Murray <spjmurray@yahoo.co.uk>
2018-06-21 11:10:43 +0100

    Version 1.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='tco',
-    version='1.0.1',
+    version='1.0.2',
     packages=setuptools.find_packages(),
     entry_points = {
         'console_scripts': [
